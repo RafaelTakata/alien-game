638e6d4c7a21e09aff660e9c515ef64028d857b9
RafaelTakata <129430952+RafaelTakata@users.noreply.github.com>
2023-09-16 19:28:08 +0100

    Add files via upload

---
+++ Alien game/sketch.js
@@ -0,0 +1,62 @@
+var bg,bgImg;
+var player, shooterImg, shooter_shooting;
+var nave1;
+
+function preload(){
+  
+  shooterImg = loadImage("assets/shooter_2.png");
+  nave1=loadImage("assets/nave1.png");
+  bgImg = loadImage("assets/espaco.avif");
+
+
+
+}
+
+function setup() {
+
+  
+  createCanvas(windowWidth,windowHeight);
+
+  //adicionando a imagem de fundo
+  bg = createSprite(displayWidth/2-20,displayHeight/2-40,20,20)
+bg.addImage(bgImg)
+bg.scale = 3.2
+
+nave = createSprite(displayWidth/2-20,displayHeight/2-40,20,20)
+nave.addImage(nave1);
+nave.scale = 0.34
+
+//criando o sprite do jogador
+player = createSprite(displayWidth-1800, displayHeight-300, 50, 50);
+ player.addImage(shooterImg)
+   player.scale = 0.15
+   player.debug = true
+   player.setCollider("rectangle",0,0,300,300)
+
+
+}
+
+function draw() {
+  background(0); 
+
+
+
+
+  //movendo o jogador para cima e para baixo e tornando o jogo compatível com dispositivos móveis usando toques
+if(keyDown("UP_ARROW")||touches.length>0){
+  player.y = player.y-30
+}
+if(keyDown("DOWN_ARROW")||touches.length>0){
+ player.y = player.y+30
+}
+
+
+//solte balas e mude a imagem do atirador para a posição de tiro quando a tecla de espaço for pressionada
+
+
+//o jogador volta à imagem original quando pararmos de pressionar a barra de espaço
+
+
+drawSprites();
+
+}
